97f4ab2de982d9f524eb358e35aed3ef1949d5a1
janupas <janupasilva2005@gmail.com>
2025-05-29 21:05:57 +0530

    Implemented the post request to create a new user

--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -1,6 +1,9 @@
-import { getAllUsersService } from '../db/queries'
+import { createNewUserService, getAllUsersService } from '../db/queries'
 import { Request, Response } from 'express'
 
+/**
+ * /api/users GET endpoint
+ */
 const getAllUsers = async (req: Request, res: Response) => {
   const users = await getAllUsersService()
 
@@ -14,6 +17,41 @@ const getAllUsers = async (req: Request, res: Response) => {
   }
 }
 
+/**
+ * /api/users POST endpoint
+ */
+const createNewUser = async (req: Request, res: Response) => {
+  const { supabase_uid, display_name } = req.body
+
+  try {
+    const newUser = await createNewUserService({
+      uid: supabase_uid,
+      name: display_name,
+    })
+
+    if (newUser) {
+      res.json({
+        success: true,
+        message: 'New User Created',
+        user: newUser,
+      })
+    } else {
+      res.json({
+        success: false,
+        message: 'An error occurred',
+      })
+    }
+  } catch (error) {
+    console.log(error)
+
+    res.json({
+      success: false,
+      error,
+    })
+  }
+}
+
 export default {
   getAllUsers,
+  createNewUser,
 }
--- apps/api/src/routes/user.route.ts
+++ apps/api/src/routes/user.route.ts
@@ -4,5 +4,6 @@ import UserController from '../controllers/user.controller'
 const router = Router()
 
 router.get('/users', UserController.getAllUsers)
+router.post('/users', UserController.createNewUser)
 
 export default router
